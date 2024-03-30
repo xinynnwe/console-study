106122ff368c0918626d7f49b0095419a93f1e44
jotyr <jotyr@SYNKULYCH>
2024-03-30 22:42:49 +0200

    Add project files.

---
+++ console study/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	
+	srand(time(NULL));
+
+	bool alreadyUsed;
+
+	const int size = 10;
+	int arr[size];
+	
+	for (int i = 0; i < size; )
+	{
+		alreadyUsed = false;
+		int randomValue = rand() % 20;
+		for (int j = 0; j < i; j++)
+		{
+			if (randomValue == arr[j])
+			{
+				alreadyUsed = true;
+				break;
+			}
+		}
+
+		if (!alreadyUsed)
+		{
+			arr[i] = randomValue;
+			i++;
+		}
+	}
+	
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+	
+}	
